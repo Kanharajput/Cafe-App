8edff2caec306d165250c7bd87f7f09494ab0610
Kanha Tomar <kanharajput91@gmail.com>
2022-01-27 16:20:24 +0530

    Spinner set in front of Phone no.'s Entry

--- app/src/main/java/com/example/specifickeyboard/OrderActivity.java
+++ app/src/main/java/com/example/specifickeyboard/OrderActivity.java
@@ -6,17 +6,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Explode;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
-    TextView txt_show_ordered_item;
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private TextView txt_show_ordered_item;
+    private int check = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        // setting onclickListener
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        // creates ArrayAdapter and layout when a default item show at top from list
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource
+                            (android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
 
         // set an exit transition
         getWindow().setEnterTransition(new Explode());
@@ -50,4 +72,16 @@ public class OrderActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        if(++check > 1) {
+            String spinnerLabel = parent.getItemAtPosition(position).toString();
+            showToast(spinnerLabel);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
